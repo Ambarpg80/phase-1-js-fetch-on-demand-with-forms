0a96c249952af28ac8c6afcb0b7bc773b4dde315
Ambar <Ambarpg80@gmail.com>
2022-09-26 12:06:59 -0400

    commit tree cbee43bccfe5cfce1979914f0f0127651ed2efdc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', (event)=>{
+  event.preventDefault()
+  console.log(event)
+  const input = document.getElementById('searchByID');
+  console.log(input.value);
 
+  fetch('http://localhost:3000/movies')
+  .then((response) => (response.json))
+  .then (data => {
+    console.log(data)
+    });
+  //recallMovie(input.value)
+  });
+}
+//function recallMovie(){
+// const something =   document.createElement('')
+//}
 document.addEventListener('DOMContentLoaded', init);
